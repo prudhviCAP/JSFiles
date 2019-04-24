15aee1271e9e8942dd732b9eb20edfd49606f397
prudhviCAP <45702943+prudhviCAP@users.noreply.github.com>
2019-04-24 14:20:24 +0530

    Add files via upload

---
+++ EmployeeSalaryReport.js
@@ -0,0 +1,114 @@
+﻿function GetEmployeeSalaryReport() {
+    var id = document.getElementById("empId").value;
+    var Employee = localStorage.getItem(id);
+    var EmpObj = JSON.parse(Employee);
+
+    var doc = document.getElementById("report");
+    var table = "<table border=1><tr><th>Id</th><th>Name</th><th>Role</th><th>DOJ</th><th>Basic</th><th>Allowances</th><th>BonusPercent</th><th>GrossAnnualSalary</th><th>MonthlyInvestment</th><th>TaxableIncome</th><th>TaxPayable</th><th>AnnualNetSalary</th></tr>";
+    table += "<tr><td>" + EmpObj.EmpId + "</td><td>" +
+        EmpObj.EmpName + "</td><td>" +
+        EmpObj.Role + "</td><td>" +
+        EmpObj.DateOfJoining + "</td><td>" +
+        EmpObj.Basic + "</td><td>" +
+        EmpObj.Allowances + "</td><td>" +
+        EmpObj.BonusPercent + "</td><td>" +
+        EmpObj.GrossAnnualSalary + "</td><td>" +
+    document.getElementById("investment").value + "</td><td>" +
+        GetTaxableIncome() + "</td><td>" +
+        GetTaxPayable() + "</td><td>" +
+        GetAnnualNetSalary() + "</td></tr>";
+    table += "</table > ";
+    doc.innerHTML = table;
+}
+function GetTaxableIncome() {
+    var id = document.getElementById("empId").value;
+    var invest = document.getElementById("investment").value;
+
+    var Employee = localStorage.getItem(id);
+    var EmpObj = JSON.parse(Employee);
+    var doj = EmpObj.DateOfJoining;
+
+    var TaxableIncome = 0;
+
+    if ((doj < '2017-03-31') || ((doj >= '2017-03-31') && (doj < '2018-03-31')) || ((doj >= '2018-03-31') && (doj < '2019-03-31'))) {
+        if ((parseFloat(invest) * 12) < 150000) {
+            TaxableIncome = parseFloat(EmpObj.GrossAnnualSalary) - (parseFloat(invest) * 12);
+        }
+        else if ((parseFloat(invest) * 12) >= 150000) {
+            TaxableIncome = parseFloat(EmpObj.GrossAnnualSalary) - 150000;
+        }
+    }
+    else if (doj >= '2019-03-31') {
+        if ((parseFloat(invest) * 12) < 200000) {
+            TaxableIncome = parseFloat(EmpObj.GrossAnnualSalary) - (parseFloat(invest) * 12);
+        }
+        else if ((parseFloat(invest) * 12) >= 150000) {
+            TaxableIncome = parseFloat(EmpObj.GrossAnnualSalary) - 200000;
+        }
+
+    }
+    return TaxableIncome;
+}
+function GetAnnualNetSalary() {
+    var taxableIncome = GetTaxableIncome();
+    var taxPayable = GetTaxPayable();
+    var annualSalary = parseFloat(taxableIncome) - parseFloat(taxPayable);
+    return annualSalary;
+}
+function GetTaxPayable() {
+    var id = document.getElementById("empId").value;
+
+    var Employee = localStorage.getItem(id);
+    var EmpObj = JSON.parse(Employee);
+    var DateOfJoin = EmpObj.DateOfJoining;
+    var doj = document.getElementById("assessment").value;
+
+    var income = GetTaxableIncome();
+    var TaxPayable = 0;
+
+    if (doj < '2017-03-31') {
+        if ((income > 250000) && (income <= 500000)) {
+            TaxPayable = (income - 250000) * 0.1;
+        }
+        else if ((income > 500000) && (income <= 1000000)) {
+            TaxPayable = ((income - 500000) * 0.2) + 25000;
+        }
+        else if (income > 1000000) {
+            TaxPayable = ((income - 1000000) * 0.3) + 125000;
+        }
+    }
+    else if ((doj >= '2017-03-31')&&(doj < '2019-03-31')) {
+        if ((income > 250000) && (income <= 500000)) {
+            TaxPayable = (income - 250000) * 0.05;
+        }
+        else if ((income > 500000) && (income <= 1000000)) {
+            TaxPayable = ((income - 500000) * 0.2) + 12500;
+        }
+        else if (income > 1000000) {
+            TaxPayable = ((income - 1000000) * 0.3) + 112500;
+        }
+    }
+    else if (doj >= '2019-03-31') {
+        if ((DateOfJoin >= '2019-04-01') && (DateOfJoin < '2020-03-31')) {
+            if ((income > 500000) && (income <= 1000000)) {
+                TaxPayable = ((income - 500000) * 0.2);
+            }
+            else if (income > 1000000) {
+                TaxPayable = ((income - 1000000) * 0.3) + 100000;
+            }
+        }
+        else {
+            if ((income > 250000) && (income <= 500000)) {
+                TaxPayable = (income - 250000) * 0.05;
+            }
+            else if ((income > 500000) && (income <= 1000000)) {
+                TaxPayable = ((income - 500000) * 0.2) + 12500;
+            }
+            else if (income > 1000000) {
+                TaxPayable = ((income - 1000000) * 0.3) + 112500;
+            }
+        }
+
+    }
+    return TaxPayable;
+}
---
+++ PopulateEmployee.js
@@ -0,0 +1,114 @@
+﻿function PopulateEmployee() {
+    var EmployeeId = document.getElementById("empId").value;
+    var EmployeeName = document.getElementById("empName").value;
+    var roleName = document.getElementById("roleName");
+    var roleIndex = roleName.options[roleName.selectedIndex].value;
+    var DOJ = document.getElementById("doj").value;
+    var allowan = document.getElementById("allowance").value;
+
+    var EmployeeJSON = {
+        EmpId: EmployeeId,
+        EmpName: EmployeeName,
+        Role: roleIndex,
+        DateOfJoining: DOJ,
+        Basic: GetBasicSalary(),
+        Allowances: allowan,
+        BonusPercent: GetBonusPercent(),
+        GrossAnnualSalary: GetGrossAnnualSalary()
+    }
+
+    localStorage.setItem(EmployeeJSON.EmpId, JSON.stringify(EmployeeJSON));
+}
+
+function DisplayAddedEmployee() {
+    var id = document.getElementById("empId").value;
+    var obj = localStorage.getItem(id);
+    console.log(obj);
+    var EmployeeObj = JSON.parse(obj);
+
+    var element = document.getElementById("display");
+    var Display = "<table border=1><tr><th>Id</th><th>Name</th><th>Role</th><th>DOJ</th><th>Basic</th></tr>";
+    Display += "<tr><td>" + EmployeeObj.EmpId + "</td><td>" +
+        EmployeeObj.EmpName + "</td><td>" +
+        EmployeeObj.Role + "</td><td>" +
+        EmployeeObj.DateOfJoining + "</td><td>" +
+        EmployeeObj.Basic + "</td></tr>";
+    Display += "</table>";
+    element.innerHTML = Display;
+}
+
+function GetBasicSalary() {
+    var roleName = document.getElementById("roleName");
+    var roleIndex = roleName.options[roleName.selectedIndex].value;
+    var result = 0;
+
+    switch (roleIndex) {
+        case 'JuniorSoftwareDeveloper':
+            result = 20000;
+            break;
+        case 'SeniorSoftwareDeveloper':
+            result = 30000;
+            break;
+        case 'JuniorQualityAnalyst':
+            result = 20000;
+            break;
+        case 'SeniorQualityAnalyst':
+            result = 30000;
+            break;
+        case 'BusinessAnalyst':
+            result = 35000;
+            break;
+        case 'TeamLead':
+            result = 50000;
+            break;
+        case 'Manager':
+            result = 100000;
+            break;
+        default:
+            document.write("Invalid Role name");
+            break;
+    }
+    return result;
+}
+
+function GetBonusPercent() {
+    var roleName = document.getElementById("roleName");
+    var roleIndex = roleName.options[roleName.selectedIndex].value;
+    var bonusPer = 0;
+
+    switch (roleIndex) {
+        case 'JuniorSoftwareDeveloper':
+            bonusPer = 5;
+            break;
+        case 'SeniorSoftwareDeveloper':
+            bonusPer = 10;
+            break;
+        case 'JuniorQualityAnalyst':
+            bonusPer = 5;
+            break;
+        case 'SeniorQualityAnalyst':
+            bonusPer = 10;
+            break;
+        case 'BusinessAnalyst':
+            bonusPer = 7;
+            break;
+        case 'TeamLead':
+            bonusPer = 15;
+            break;
+        case 'Manager':
+            bonusPer = 20;
+            break;
+        default:
+            document.write("Invalid Role name");
+            break;
+    }
+    return bonusPer;
+}
+
+function GetGrossAnnualSalary() {
+    var allowance = document.getElementById("allowance").value;
+    var basic = GetBasicSalary();
+    var gross = GetBonusPercent() / 100;
+    var grossAnnual = (parseFloat(basic)+parseFloat(allowance) + parseFloat(basic * gross)) * 12;
+    return grossAnnual;
+}
